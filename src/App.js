e146bc7758ad136f583bd0091ae3dcb5cb543bae
Garrett Ross <garrettross@gGarrettRoss.local>
2021-09-01 10:06:54 -0500

    assessment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+
+  render() {
+    return (
+      <div className="App">
+        {/* Create state for 
+          products: which is going to be an array -
+          each item should have a name of the item, a product number, an id, and a price.
+  
+          loading: which is going to be a boolean on if the component is loading or not
+  
+          - Create a component for Products, loop through the Products, and output the list in 
+          another component called ProductItems
+        */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
